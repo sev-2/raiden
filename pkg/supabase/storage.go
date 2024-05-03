3a28614315d2e5fc67a69cf5d461d1808a5a0ede
Ach.Vani Ardiansyah <achvani@refactory.id>
2024-05-03 19:34:24 +0700

    feature : decorate error for manage storage function

--- pkg/supabase/storage.go
+++ pkg/supabase/storage.go
@@ -36,85 +36,96 @@ func getBucketUrl(cfg *raiden.Config) string {
 }
 
 func GetBuckets(cfg *raiden.Config) (buckets []objects.Bucket, err error) {
-	StorageLogger.Debug("fetch all bucket from cloud")
-	return net.Get[[]objects.Bucket](getBucketUrl(cfg), net.DefaultTimeout, DefaultAuthInterceptor(cfg.ServiceKey, cfg.ServiceKey), nil)
+	return decorateActionWithDataErr("fetch", "storage", func() ([]objects.Bucket, error) {
+		StorageLogger.Debug("fetch all bucket from cloud")
+		return net.Get[[]objects.Bucket](getBucketUrl(cfg), net.DefaultTimeout, DefaultAuthInterceptor(cfg.ServiceKey, cfg.ServiceKey), nil)
+	})
 }
 
 func GetBucket(cfg *raiden.Config, bucketId string) (buckets objects.Bucket, err error) {
-	StorageLogger.Debug("fetch bucket by name from cloud")
-	url := fmt.Sprintf("%s/%s", getBucketUrl(cfg), bucketId)
-	return net.Get[objects.Bucket](url, net.DefaultTimeout, DefaultAuthInterceptor(cfg.ServiceKey, cfg.ServiceKey), nil)
+	return decorateActionWithDataErr("fetch", "storage", func() (objects.Bucket, error) {
+		StorageLogger.Debug("fetch bucket by name from cloud")
+		url := fmt.Sprintf("%s/%s", getBucketUrl(cfg), bucketId)
+		return net.Get[objects.Bucket](url, net.DefaultTimeout, DefaultAuthInterceptor(cfg.ServiceKey, cfg.ServiceKey), nil)
+	})
+
 }
 
 func CreateBucket(cfg *raiden.Config, param objects.Bucket) (bucket objects.Bucket, err error) {
-	StorageLogger.Debug("start create storage", "name", param.Name)
-	byteData, err := json.Marshal(param)
-	if err != nil {
-		return bucket, err
-	}
+	return decorateActionWithDataErr("create", "storage", func() (objects.Bucket, error) {
+		StorageLogger.Debug("start create storage", "name", param.Name)
+		byteData, err := json.Marshal(param)
+		if err != nil {
+			return bucket, err
+		}
 
-	res, err := net.Post[CreateBucketSuccessResponse](
-		getBucketUrl(cfg), byteData, net.DefaultTimeout, DefaultAuthInterceptor(cfg.ServiceKey, cfg.ServiceKey), nil,
-	)
+		res, err := net.Post[CreateBucketSuccessResponse](
+			getBucketUrl(cfg), byteData, net.DefaultTimeout, DefaultAuthInterceptor(cfg.ServiceKey, cfg.ServiceKey), nil,
+		)
 
-	if err != nil {
-		return bucket, err
-	}
-	StorageLogger.Debug("finish create storage", "name", param.Name)
-	return GetBucket(cfg, res.Name)
+		if err != nil {
+			return bucket, err
+		}
+		StorageLogger.Debug("finish create storage", "name", param.Name)
+		return GetBucket(cfg, res.Name)
+	})
 }
 
 func UpdateBucket(cfg *raiden.Config, param objects.Bucket, updateItem objects.UpdateBucketParam) error {
-	StorageLogger.Debug("start update storage", "name", param.Name)
+	return decorateActionErr("update", "storage", func() error {
+		StorageLogger.Debug("start update storage", "name", param.Name)
 
-	// just return, nothing to be processed
-	if len(updateItem.ChangeItems) == 0 {
-		return nil
-	}
-
-	// build update payload
-	updateBucket := objects.Bucket{}
-	for i := range updateItem.ChangeItems {
-		u := updateItem.ChangeItems[i]
-		switch u {
-		case objects.UpdateBucketIsPublic:
-			updateBucket.Public = param.Public
-		case objects.UpdateBucketFileSizeLimit:
-			updateBucket.FileSizeLimit = param.FileSizeLimit
-		case objects.UpdateBucketAllowedMimeTypes:
-			updateBucket.AllowedMimeTypes = param.AllowedMimeTypes
+		// just return, nothing to be processed
+		if len(updateItem.ChangeItems) == 0 {
+			return nil
 		}
-	}
 
-	// build request
-	byteData, err := json.Marshal(updateBucket)
-	if err != nil {
+		// build update payload
+		updateBucket := objects.Bucket{}
+		for i := range updateItem.ChangeItems {
+			u := updateItem.ChangeItems[i]
+			switch u {
+			case objects.UpdateBucketIsPublic:
+				updateBucket.Public = param.Public
+			case objects.UpdateBucketFileSizeLimit:
+				updateBucket.FileSizeLimit = param.FileSizeLimit
+			case objects.UpdateBucketAllowedMimeTypes:
+				updateBucket.AllowedMimeTypes = param.AllowedMimeTypes
+			}
+		}
+
+		// build request
+		byteData, err := json.Marshal(updateBucket)
+		if err != nil {
+			return err
+		}
+
+		url := fmt.Sprintf("%s/%s", getBucketUrl(cfg), param.ID)
+		_, err = net.Put[DefaultBucketSuccessResponse](
+			url, byteData, net.DefaultTimeout, DefaultAuthInterceptor(cfg.ServiceKey, cfg.ServiceKey), nil,
+		)
+		StorageLogger.Debug("finish update storage", "name", param.Name)
 		return err
-	}
-
-	url := fmt.Sprintf("%s/%s", getBucketUrl(cfg), param.ID)
-	_, err = net.Put[DefaultBucketSuccessResponse](
-		url, byteData, net.DefaultTimeout, DefaultAuthInterceptor(cfg.ServiceKey, cfg.ServiceKey), nil,
-	)
-	StorageLogger.Debug("finish update storage", "name", param.Name)
-	return err
+	})
 }
 
 func DeleteBucket(cfg *raiden.Config, param objects.Bucket) (err error) {
-	StorageLogger.Debug("start delete storage", "name", param.Name)
+	return decorateActionErr("delete", "storage", func() error {
+		StorageLogger.Debug("start delete storage", "name", param.Name)
 
-	deleteReqInterceptor := func(req *http.Request) error {
-		req.Header.Set("apiKey", cfg.ServiceKey)
-		req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", cfg.ServiceKey))
-		req.Header.Set("Content-Type", "")
-		return nil
-	}
+		deleteReqInterceptor := func(req *http.Request) error {
+			req.Header.Set("apiKey", cfg.ServiceKey)
+			req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", cfg.ServiceKey))
+			req.Header.Set("Content-Type", "")
+			return nil
+		}
 
-	url := fmt.Sprintf("%s/%s", getBucketUrl(cfg), param.ID)
-	_, err = net.Delete[DefaultBucketSuccessResponse](
-		url, nil, net.DefaultTimeout, deleteReqInterceptor, nil,
-	)
+		url := fmt.Sprintf("%s/%s", getBucketUrl(cfg), param.ID)
+		_, err = net.Delete[DefaultBucketSuccessResponse](
+			url, nil, net.DefaultTimeout, deleteReqInterceptor, nil,
+		)
 
-	StorageLogger.Debug("finish delete storage", "name", param.Name)
-	return err
+		StorageLogger.Debug("finish delete storage", "name", param.Name)
+		return err
+	})
 }
