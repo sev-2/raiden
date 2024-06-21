aab249c40f7f29b052209a45381a00397f57443a
Mulia Nasution <mul14@users.noreply.github.com>
2024-06-21 14:06:06 +0700

    feat: prevent invalid columns to be selected

--- pkg/suparest/select.go
+++ pkg/suparest/select.go
@@ -1,6 +1,47 @@
 package suparest
 
-func (q Query) Select(c ...string) (model *Query) {
-	q.Columns = c
+import (
+	"fmt"
+	"reflect"
+	"strings"
+
+	"github.com/sev-2/raiden"
+)
+
+func (q Query) Select(columns ...string) (model *Query) {
+	validSet := make(map[string]bool)
+
+	for _, v := range GetColumnList(q.model) {
+		validSet[v] = true
+	}
+
+	for _, v := range columns {
+		if !validSet[v] {
+			raiden.Fatal(fmt.Sprintf("invalid column: %s is not available on %s table", v, GetTable(q.model)))
+		}
+	}
+
+	q.Columns = columns
 	return &q
+}
+
+func GetColumnList(m interface{}) []string {
+	r := reflect.TypeOf(m)
+
+	var columns []string
+
+	for i := 0; i < r.NumField(); i++ {
+		field := r.Field(i)
+		tag := field.Tag.Get("column")
+		if tag != "" {
+			for _, t := range strings.Split(tag, ";") {
+				if strings.HasPrefix(t, "name:") {
+					name := strings.TrimPrefix(t, "name:")
+					columns = append(columns, name)
+				}
+			}
+		}
+	}
+
+	return columns
 }
