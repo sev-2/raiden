73f0ba971ad6127af7ddc6aef49bd35ca0185037
Mulia Nasution <mul14@users.noreply.github.com>
2024-07-03 16:18:32 +0700

    feat: refactor relation

--- pkg/suparest/relation.go
+++ pkg/suparest/relation.go
@@ -2,18 +2,72 @@ package suparest
 
 import (
 	"fmt"
+	"reflect"
+	"sort"
 	"strings"
+
+	"github.com/sev-2/raiden"
+	"github.com/sev-2/raiden/pkg/resource"
 )
 
-func (q *Query) With(m interface{}, cols []string) *Query {
-	table := GetTable(m)
+func (q *Query) With(r string, columns map[string][]string) *Query {
 
-	columns := strings.Join(cols, ",")
+	relations := strings.Split(r, ".")
 
-	q.Columns = append(
-		q.Columns,
-		fmt.Sprintf("%s(%s)", table, columns),
-	)
+	if len(relations) > 3 {
+		raiden.Fatal("unsupported nested relations more than 3 levels")
+	}
+
+	regs := make(map[string]bool)
+
+	for _, m := range resource.RegisteredModels {
+		registeredModel := reflect.TypeOf(m).Elem().Name()
+		regs[registeredModel] = true
+	}
+
+	for _, m := range relations {
+		if !regs[m] {
+			raiden.Fatal(fmt.Sprintf("invalid model name: %s", m))
+		}
+	}
+
+	var selects []string
+
+	for _, r := range reverseSortString(relations) {
+		table := GetTable(findModel(resource.RegisteredModels, r))
+		cols := strings.Join(columns[r], ",")
+
+		if len(cols) == 0 {
+			cols = "*"
+		}
+
+		if len(selects) > 0 {
+			lastQuery := selects[len(selects)-1]
+			selects[len(selects)-1] = fmt.Sprintf("%s(%s,%s)", table, cols, lastQuery)
+		} else {
+			selects = append(selects, fmt.Sprintf("%s(%s)", table, cols))
+		}
+	}
+
+	q.Columns = append(q.Columns, selects...)
 
 	return q
+}
+
+func findModel(models []interface{}, targetName string) interface{} {
+	for _, m := range models {
+		if reflect.TypeOf(m).Elem().Name() == targetName {
+			return m
+		}
+	}
+
+	return nil
+}
+
+func reverseSortString(n []string) []string {
+	sort.Slice(n, func(i, j int) bool {
+		return n[i] < n[j]
+	})
+
+	return n
 }
