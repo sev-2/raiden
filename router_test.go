bc2398816d13f1297e5eed947c7fec3d6f4167ce
Refactory <toopay@taufanaditya.com>
2024-07-15 05:25:18 +0700

    feat(test): router.go

---
+++ router_test.go
@@ -0,0 +1,117 @@
+package raiden_test
+
+// write unit test for router.go file
+import (
+	"os"
+	"testing"
+
+	"github.com/sev-2/raiden"
+	"github.com/stretchr/testify/assert"
+	"go.opentelemetry.io/otel/trace"
+)
+
+func loadConfig() *raiden.Config {
+	// Create a temporary config file with valid content
+	file, err := os.CreateTemp("", "config*.yaml")
+	if err != nil {
+		return nil
+	}
+	defer os.Remove(file.Name())
+
+	configContent := `
+ACCESS_TOKEN: "test-access-token"
+ANON_KEY: "test-anon-key"
+BREAKER_ENABLE: true
+CORS_ALLOWED_ORIGINS: "*"
+CORS_ALLOWED_METHODS: "GET,POST"
+CORS_ALLOWED_HEADERS: "Content-Type"
+CORS_ALLOWED_CREDENTIALS: true
+DEPLOYMENT_TARGET: "cloud"
+ENVIRONMENT: "production"
+PROJECT_ID: "test-project-id"
+PROJECT_NAME: "test-project"
+SERVICE_KEY: "test-service-key"
+SERVER_HOST: "127.0.0.1"
+SERVER_PORT: "8080"
+SUPABASE_API_URL: "http://test-supabase-api-url"
+SUPABASE_API_BASE_PATH: "/api"
+SUPABASE_PUBLIC_URL: "http://test-supabase-public-url"
+SCHEDULE_STATUS: "on"
+TRACE_ENABLE: true
+TRACE_COLLECTOR: "collector"
+TRACE_COLLECTOR_ENDPOINT: "endpoint"
+VERSION: "2.0.0"
+`
+	if _, err := file.WriteString(configContent); err != nil {
+		return nil
+	}
+	file.Close()
+
+	path := file.Name()
+	config, err := raiden.LoadConfig(&path)
+	if err != nil {
+		return nil
+	}
+	return config
+}
+
+func TestNewRouter(t *testing.T) {
+	conf := loadConfig()
+	router := raiden.NewRouter(conf)
+	assert.NotNil(t, router)
+}
+
+func TestRouter_SetJobChan_NotNil(t *testing.T) {
+	conf := loadConfig()
+	router := raiden.NewRouter(conf)
+	router.SetJobChan(make(chan raiden.JobParams))
+}
+
+func TestRouter_SetTracer_NotNil(t *testing.T) {
+	conf := loadConfig()
+	router := raiden.NewRouter(conf)
+	router.SetTracer(trace.NewNoopTracerProvider().Tracer("raiden"))
+}
+
+func TestRouter_RegisterMiddlewares(t *testing.T) {
+	conf := loadConfig()
+	router := raiden.NewRouter(conf)
+	router.RegisterMiddlewares([]raiden.MiddlewareFn{})
+}
+
+func TestRouter_AddRoute(t *testing.T) {
+	conf := loadConfig()
+	router := raiden.NewRouter(conf)
+	route := raiden.Route{Methods: []string{"GET", "POST"}, Path: "/"}
+	routes := []*raiden.Route{&route}
+	router.Register(routes)
+}
+
+func TestRouter_GetRegisteredRoutes(t *testing.T) {
+	conf := loadConfig()
+	router := raiden.NewRouter(conf)
+	route := raiden.Route{Methods: []string{"GET", "POST"}, Path: "/"}
+	routes := []*raiden.Route{&route}
+	router.Register(routes)
+	allRegisteredRoutes := router.GetRegisteredRoutes()
+	assert.NotNil(t, allRegisteredRoutes)
+}
+
+func TestRouter_PrintRoutes(t *testing.T) {
+	conf := loadConfig()
+	router := raiden.NewRouter(conf)
+	route := raiden.Route{Methods: []string{"GET", "POST"}, Path: "/"}
+	routes := []*raiden.Route{&route}
+	router.Register(routes)
+	router.PrintRegisteredRoute()
+}
+
+func TestRouter_GetHandler(t *testing.T) {
+	conf := loadConfig()
+	router := raiden.NewRouter(conf)
+	route := raiden.Route{Methods: []string{"GET", "POST"}, Path: "/"}
+	routes := []*raiden.Route{&route}
+	router.Register(routes)
+	handler := router.GetHandler()
+	assert.NotNil(t, handler)
+}
