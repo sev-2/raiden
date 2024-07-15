c2b87949d88276a2a394b8a2121eb66d118382a8
Refactory <toopay@taufanaditya.com>
2024-07-16 00:12:32 +0700

    feat(test): pkg/generator/config.go

---
+++ pkg/generator/config_test.go
@@ -0,0 +1,20 @@
+package generator_test
+
+import (
+	"os"
+	"testing"
+
+	"github.com/sev-2/raiden/pkg/generator"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGenerateConfig(t *testing.T) {
+	dir, err := os.MkdirTemp("", "apply")
+	assert.NoError(t, err)
+
+	conf := loadConfig()
+
+	err1 := generator.GenerateConfig(dir, conf, generator.GenerateFn(generator.Generate))
+	assert.NoError(t, err1)
+	assert.FileExists(t, dir+"/configs/app.yaml")
+}
