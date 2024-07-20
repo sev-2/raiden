249d853c64d4cf08e68eaa8780983d5a18afc422
Refactory <toopay@taufanaditya.com>
2024-07-20 18:36:57 +0700

    feat(test): pkg/generator/config.go

--- pkg/generator/config_test.go
+++ pkg/generator/config_test.go
@@ -4,6 +4,7 @@ import (
 	"os"
 	"testing"
 
+	"github.com/sev-2/raiden"
 	"github.com/sev-2/raiden/pkg/generator"
 	"github.com/stretchr/testify/assert"
 )
@@ -12,7 +13,16 @@ func TestGenerateConfig(t *testing.T) {
 	dir, err := os.MkdirTemp("", "config")
 	assert.NoError(t, err)
 
-	conf := loadConfig()
+	conf := &raiden.Config{
+		DeploymentTarget:    raiden.DeploymentTargetSelfHosted,
+		ProjectId:           "test-project-id",
+		ProjectName:         "test-project",
+		SupabaseApiBasePath: "/v1",
+		SupabaseApiUrl:      "http://supabase.local.com",
+		SupabasePublicUrl:   "http://supabase.local.com",
+		ServerPort:          "8080",
+		ServerHost:          "localhost",
+	}
 
 	err1 := generator.GenerateConfig(dir, conf, generator.GenerateFn(generator.Generate))
 	assert.NoError(t, err1)
