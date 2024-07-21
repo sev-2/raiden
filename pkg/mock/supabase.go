2c2ec0b6e285769cb10851b713be2ca6db08b201
Refactory <toopay@taufanaditya.com>
2024-07-22 00:08:33 +0700

    feat(mock): set matcher to avoid generalization

--- pkg/mock/supabase.go
+++ pkg/mock/supabase.go
@@ -31,187 +31,213 @@ func (m *MockSupabase) MockFindProjectWithExpectedResponse(httpCode int, project
 	var url = fmt.Sprintf("%s%s/projects", m.Cfg.SupabaseApiUrl, m.Cfg.SupabaseApiBasePath)
 	projects := []objects.Project{project}
 
-	return registerMock(method, url, httpCode, projects)
+	return registerMock(m.Cfg, "findProject", method, url, httpCode, projects)
 }
 
 func (m *MockSupabase) MockGetTablesWithExpectedResponse(httpCode int, tables []objects.Table) error {
-	method, url := getMethodAndUrl(m.Cfg, "getTables")
+	actionType, method, url := getMethodAndUrl(m.Cfg, "getTables")
 
-	return registerMock(method, url, httpCode, tables)
+	return registerMock(m.Cfg, actionType, method, url, httpCode, tables)
 }
 
 func (m *MockSupabase) MockGetTableByNameWithExpectedResponse(httpCode int, table objects.Table) error {
-	method, url := getMethodAndUrl(m.Cfg, "getTables")
+	actionType, method, url := getMethodAndUrl(m.Cfg, "getTables")
 
-	return registerMock(method, url, httpCode, []objects.Table{table})
+	return registerMock(m.Cfg, actionType, method, url, httpCode, []objects.Table{table})
 }
 
 func (m *MockSupabase) MockCreateTableWithExpectedResponse(httpCode int, table objects.Table) error {
-	method, url := getMethodAndUrl(m.Cfg, "common")
+	actionType, method, url := getMethodAndUrl(m.Cfg, "common")
 
-	return registerMock(method, url, httpCode, []objects.Table{table})
+	return registerMock(m.Cfg, actionType, method, url, httpCode, []objects.Table{table})
 }
 
 func (m *MockSupabase) MockUpdateTableWithExpectedResponse(httpCode int) error {
-	method, url := getMethodAndUrl(m.Cfg, "common")
+	actionType, method, url := getMethodAndUrl(m.Cfg, "common")
 
-	return registerMock(method, url, httpCode, objects.Table{})
+	return registerMock(m.Cfg, actionType, method, url, httpCode, objects.Table{})
 }
 
 func (m *MockSupabase) MockDeleteTableWithExpectedResponse(httpCode int) error {
-	method, url := getMethodAndUrl(m.Cfg, "common")
+	actionType, method, url := getMethodAndUrl(m.Cfg, "common")
 
-	return registerMock(method, url, httpCode, objects.Table{})
+	return registerMock(m.Cfg, actionType, method, url, httpCode, objects.Table{})
 }
 
 func (m *MockSupabase) MockGetRolesWithExpectedResponse(httpCode int, roles []objects.Role) error {
-	method, url := getMethodAndUrl(m.Cfg, "getRoles")
+	actionType, method, url := getMethodAndUrl(m.Cfg, "getRoles")
 
-	return registerMock(method, url, httpCode, roles)
+	return registerMock(m.Cfg, actionType, method, url, httpCode, roles)
 }
 
 func (m *MockSupabase) MockGetRoleByNameWithExpectedResponse(httpCode int, role objects.Role) error {
-	method, url := getMethodAndUrl(m.Cfg, "common")
+	actionType, method, url := getMethodAndUrl(m.Cfg, "common")
 
-	return registerMock(method, url, httpCode, []objects.Role{role})
+	return registerMock(m.Cfg, actionType, method, url, httpCode, []objects.Role{role})
 }
 
 func (m *MockSupabase) MockCreateRoleWithExpectedResponse(httpCode int, role objects.Role) error {
-	method, url := getMethodAndUrl(m.Cfg, "common")
+	actionType, method, url := getMethodAndUrl(m.Cfg, "common")
 
-	return registerMock(method, url, httpCode, []objects.Role{role})
+	return registerMock(m.Cfg, actionType, method, url, httpCode, []objects.Role{role})
 }
 
 func (m *MockSupabase) MockUpdateRoleWithExpectedResponse(httpCode int) error {
-	method, url := getMethodAndUrl(m.Cfg, "common")
+	actionType, method, url := getMethodAndUrl(m.Cfg, "common")
 
-	return registerMock(method, url, httpCode, objects.Role{})
+	return registerMock(m.Cfg, actionType, method, url, httpCode, objects.Role{})
 }
 
 func (m *MockSupabase) MockDeleteRoleWithExpectedResponse(httpCode int) error {
-	method, url := getMethodAndUrl(m.Cfg, "common")
+	actionType, method, url := getMethodAndUrl(m.Cfg, "common")
 
-	return registerMock(method, url, httpCode, objects.Role{})
+	return registerMock(m.Cfg, actionType, method, url, httpCode, objects.Role{})
 }
 
 func (m *MockSupabase) MockGetPoliciesWithExpectedResponse(httpCode int, policies []objects.Policy) error {
-	method, url := getMethodAndUrl(m.Cfg, "getPolicies")
+	actionType, method, url := getMethodAndUrl(m.Cfg, "getPolicies")
 
-	return registerMock(method, url, httpCode, policies)
+	return registerMock(m.Cfg, actionType, method, url, httpCode, policies)
 }
 
 func (m *MockSupabase) MockGetPolicyByNameWithExpectedResponse(httpCode int, policy objects.Policy) error {
-	method, url := getMethodAndUrl(m.Cfg, "common")
+	actionType, method, url := getMethodAndUrl(m.Cfg, "common")
 
-	return registerMock(method, url, httpCode, []objects.Policy{policy})
+	return registerMock(m.Cfg, actionType, method, url, httpCode, []objects.Policy{policy})
 }
 
 func (m *MockSupabase) MockCreatePolicyWithExpectedResponse(httpCode int, policy objects.Policy) error {
-	method, url := getMethodAndUrl(m.Cfg, "common")
+	actionType, method, url := getMethodAndUrl(m.Cfg, "common")
 
-	return registerMock(method, url, httpCode, []objects.Policy{policy})
+	return registerMock(m.Cfg, actionType, method, url, httpCode, []objects.Policy{policy})
 }
 
 func (m *MockSupabase) MockUpdatePolicyWithExpectedResponse(httpCode int) error {
-	method, url := getMethodAndUrl(m.Cfg, "common")
+	actionType, method, url := getMethodAndUrl(m.Cfg, "common")
 
-	return registerMock(method, url, httpCode, objects.Policy{})
+	return registerMock(m.Cfg, actionType, method, url, httpCode, objects.Policy{})
 }
 
 func (m *MockSupabase) MockDeletePolicyWithExpectedResponse(httpCode int) error {
-	method, url := getMethodAndUrl(m.Cfg, "common")
+	actionType, method, url := getMethodAndUrl(m.Cfg, "common")
 
-	return registerMock(method, url, httpCode, objects.Policy{})
+	return registerMock(m.Cfg, actionType, method, url, httpCode, objects.Policy{})
 }
 
 func (m *MockSupabase) MockGetFunctionsWithExpectedResponse(httpCode int, functions []objects.Function) error {
-	method, url := getMethodAndUrl(m.Cfg, "getFunctions")
+	actionType, method, url := getMethodAndUrl(m.Cfg, "getFunctions")
 
-	return registerMock(method, url, httpCode, functions)
+	return registerMock(m.Cfg, actionType, method, url, httpCode, functions)
 }
 
 func (m *MockSupabase) MockGetFunctionByNameWithExpectedResponse(httpCode int, function objects.Function) error {
-	method, url := getMethodAndUrl(m.Cfg, "postQuery")
+	actionType, method, url := getMethodAndUrl(m.Cfg, "postQuery")
 
-	return registerMock(method, url, httpCode, []objects.Function{function})
+	return registerMock(m.Cfg, actionType, method, url, httpCode, []objects.Function{function})
 }
 
 func (m *MockSupabase) MockCreateFunctionWithExpectedResponse(httpCode int, function objects.Function) error {
-	method, url := getMethodAndUrl(m.Cfg, "postQuery")
+	actionType, method, url := getMethodAndUrl(m.Cfg, "postQuery")
 
-	return registerMock(method, url, httpCode, []objects.Function{function})
+	return registerMock(m.Cfg, actionType, method, url, httpCode, []objects.Function{function})
 }
 
 func (m *MockSupabase) MockUpdateFunctionWithExpectedResponse(httpCode int) error {
-	method, url := getMethodAndUrl(m.Cfg, "postQuery")
+	actionType, method, url := getMethodAndUrl(m.Cfg, "postQuery")
 
-	return registerMock(method, url, httpCode, objects.Function{})
+	return registerMock(m.Cfg, actionType, method, url, httpCode, objects.Function{})
 }
 
 func (m *MockSupabase) MockDeleteFunctionWithExpectedResponse(httpCode int) error {
-	method, url := getMethodAndUrl(m.Cfg, "postQuery")
+	actionType, method, url := getMethodAndUrl(m.Cfg, "postQuery")
 
-	return registerMock(method, url, httpCode, objects.Function{})
+	return registerMock(m.Cfg, actionType, method, url, httpCode, objects.Function{})
 }
 
 func (m *MockSupabase) MockAdminUpdateUserDataWithExpectedResponse(httpCode int, user objects.User) error {
-	method, url := getMethodAndUrl(m.Cfg, "adminUpdateUserData")
+	actionType, method, url := getMethodAndUrl(m.Cfg, "adminUpdateUserData")
 
-	return registerMock(method, url, httpCode, user)
+	return registerMock(m.Cfg, actionType, method, url, httpCode, user)
 }
 
 func (m *MockSupabase) MockGetBucketsWithExpectedResponse(httpCode int, data interface{}) error {
-	method, url := getMethodAndUrl(m.Cfg, "getBuckets")
+	actionType, method, url := getMethodAndUrl(m.Cfg, "getBuckets")
 
-	return registerMock(method, url, httpCode, data)
+	return registerMock(m.Cfg, actionType, method, url, httpCode, data)
 }
 
 func (m *MockSupabase) MockGetBucketByNameWithExpectedResponse(httpCode int, bucket objects.Bucket) error {
-	method, url := getMethodAndUrl(m.Cfg, "getBuckets")
+	actionType, method, url := getMethodAndUrl(m.Cfg, "getBuckets")
 
-	return registerMock(method, url+"/"+bucket.Name, httpCode, bucket)
+	return registerMock(m.Cfg, actionType, method, url+"/"+bucket.Name, httpCode, bucket)
 }
 
 func (m *MockSupabase) MockCreateBucketsWithExpectedResponse(httpCode int, data interface{}) error {
-	method, url := getMethodAndUrl(m.Cfg, "createBucket")
+	actionType, method, url := getMethodAndUrl(m.Cfg, "createBucket")
 
-	return registerMock(method, url, httpCode, data)
+	return registerMock(m.Cfg, actionType, method, url, httpCode, data)
 }
 
 func (m *MockSupabase) MockDeleteBucketsWithExpectedResponse(httpCode int) error {
-	method, url := getMethodAndUrl(m.Cfg, "deleteBucket")
+	actionType, method, url := getMethodAndUrl(m.Cfg, "deleteBucket")
 
-	return registerMock(method, url, httpCode, supabase.DefaultBucketSuccessResponse{})
+	return registerMock(m.Cfg, actionType, method, url, httpCode, supabase.DefaultBucketSuccessResponse{})
 }
 
-func registerMock(method, url string, httpCode int, data interface{}) error {
+func registerMock(cfg *raiden.Config, actionType string, method string, url string, httpCode int, data interface{}) error {
 	jsonData, err := json.Marshal(data)
 	if err != nil {
 		return err
 	}
 	httpmock.RegisterResponder(method, url, httpmock.NewStringResponder(httpCode, string(jsonData)))
+
+	if cfg.DeploymentTarget == raiden.DeploymentTargetCloud {
+		switch actionType {
+		case "getTables":
+			httpmock.RegisterMatcherResponder(method, url,
+				httpmock.BodyContainsString("join pg_namespace nsa on csa.relnamespace = nsa.oid"),
+				httpmock.NewStringResponder(httpCode, string(jsonData)))
+		case "getRoles":
+			httpmock.RegisterMatcherResponder(method, url,
+				httpmock.BodyContainsString("rolname AS name"),
+				httpmock.NewStringResponder(httpCode, string(jsonData)))
+		case "getPolicies":
+			httpmock.RegisterMatcherResponder(method, url,
+				httpmock.BodyContainsString("pol.polname AS name"),
+				httpmock.NewStringResponder(httpCode, string(jsonData)))
+		case "getFunctions":
+			httpmock.RegisterMatcherResponder(method, url,
+				httpmock.BodyContainsString("pg_namespace n on f.pronamespace = n.oid"),
+				httpmock.NewStringResponder(httpCode, string(jsonData)))
+		}
+	}
+
 	return nil
 }
 
-func getMethodAndUrl(cfg *raiden.Config, actionType string) (string, string) {
+func getMethodAndUrl(cfg *raiden.Config, actionType string) (string, string, string) {
 	var method string
 	var url string
 
+	action := actionType
+
 	if cfg.DeploymentTarget == raiden.DeploymentTargetCloud {
-		method = "POST"
-		url = fmt.Sprintf("%s/v1/projects/%s/database/query", cfg.SupabaseApiUrl, cfg.ProjectId)
-		if actionType == "adminUpdateUserData" {
+		switch actionType {
+		case "adminUpdateUserData":
 			method = "PUT"
 			url = fmt.Sprintf("%s/auth/v1/admin/users/user-id", cfg.SupabaseApiUrl)
-		} else if actionType == "getBuckets" {
+		case "getBuckets":
 			method = "GET"
 			url = fmt.Sprintf("%s/storage/v1/bucket", cfg.SupabaseApiUrl)
-		} else if actionType == "createBucket" {
+		case "createBucket":
 			method = "POST"
 			url = fmt.Sprintf("%s/storage/v1/bucket", cfg.SupabaseApiUrl)
-		} else if actionType == "deleteBucket" {
+		case "deleteBucket":
 			method = "DELETE"
 			url = fmt.Sprintf("%s/storage/v1/bucket/", cfg.SupabaseApiUrl)
+		default:
+			method = "POST"
+			url = fmt.Sprintf("%s/v1/projects/%s/database/query", cfg.SupabaseApiUrl, cfg.ProjectId)
 		}
 	} else {
 		switch actionType {
@@ -245,5 +271,5 @@ func getMethodAndUrl(cfg *raiden.Config, actionType string) (string, string) {
 		}
 	}
 
-	return method, url
+	return action, method, url
 }
