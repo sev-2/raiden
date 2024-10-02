f5d991a65c9217bbd4a68a194cddd3a18e6dbfff
Taufan Adhitya <toopay@refactory.id>
2024-10-02 14:19:03 +0700

    feat: refactor relation utility

--- pkg/db/utils.go
+++ pkg/db/utils.go
@@ -3,53 +3,11 @@ package db
 import (
 	"log"
 	"os"
-	"reflect"
 	"sort"
 	"strings"
 
 	"github.com/sev-2/raiden"
-	"github.com/sev-2/raiden/pkg/resource"
 )
-
-func findModel(targetName string) interface{} {
-	for _, m := range resource.RegisteredModels {
-		t := reflect.TypeOf(m)
-
-		if t.Kind() == reflect.Ptr {
-			t = t.Elem()
-		}
-
-		if t.Name() == targetName {
-			return m
-		}
-	}
-
-	return nil
-}
-
-func findModelByTable(table string) string {
-	for _, m := range resource.RegisteredModels {
-
-		t := reflect.TypeOf(m)
-
-		if t.Kind() == reflect.Ptr {
-			t = t.Elem()
-		}
-
-		for i := 0; i < t.NumField(); i++ {
-			field := t.Field(i)
-			if field.Name == "Metadata" {
-				tableName := field.Tag.Get("tableName")
-				if tableName == table {
-					parts := strings.Split(t.String(), ".")
-					return parts[len(parts)-1]
-				}
-			}
-		}
-	}
-
-	return ""
-}
 
 func getConfig() *raiden.Config {
 	currentDir, err := os.Getwd()
