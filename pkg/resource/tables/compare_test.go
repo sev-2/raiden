f6afad720cb485ac6611e7f675081c52a02c935f
Taufan Adhitya <toopay@refactory.id>
2024-10-22 15:46:57 +0700

    test: without indexes

--- pkg/resource/tables/compare_test.go
+++ pkg/resource/tables/compare_test.go
@@ -149,3 +149,79 @@ func TestCompareItem(t *testing.T) {
 	assert.Equal(t, []objects.UpdateColumnType{objects.UpdateColumnNullable}, diffResult.DiffItems.ChangeColumnItems[0].UpdateItems)
 	assert.Equal(t, []objects.UpdateColumnType{objects.UpdateColumnNullable}, diffResult.DiffItems.ChangeColumnItems[1].UpdateItems)
 }
+
+func TestCompareItemWithoutIndex(t *testing.T) {
+
+	source := objects.Table{
+		ID:          1,
+		Name:        "table1",
+		Schema:      "public",
+		RLSEnabled:  true,
+		RLSForced:   true,
+		PrimaryKeys: []objects.PrimaryKey{{Name: "id", Schema: "public", TableName: "table1"}},
+		Columns: []objects.Column{
+			{Name: "id", DataType: "int", IsNullable: false},
+			{Name: "name", DataType: "varchar", IsNullable: true},
+			{Name: "nullable", DataType: "varchar", IsNullable: true},
+			{Name: "changeable", DataType: "varchar", IsNullable: true},
+			{Name: "uniqueness", DataType: "varchar", IsNullable: false, IsUnique: true},
+			{Name: "identity", DataType: "varchar", IsNullable: false, IsIdentity: true},
+		},
+		Relationships: []objects.TablesRelationship{
+			{
+				ConstraintName:    "constraint1",
+				SourceSchema:      "public",
+				SourceTableName:   "table1",
+				SourceColumnName:  "id",
+				TargetTableSchema: "public",
+				TargetTableName:   "table2",
+				TargetColumnName:  "id",
+			},
+		},
+	}
+
+	target := objects.Table{
+		ID:          1,
+		Name:        "table1_updated",
+		Schema:      "private",
+		RLSEnabled:  false,
+		RLSForced:   false,
+		PrimaryKeys: []objects.PrimaryKey{{Name: "id", Schema: "public", TableName: "table1"}},
+		Columns: []objects.Column{
+			{Name: "id", DataType: "int", IsNullable: false},
+			{Name: "name", DataType: "varchar", IsNullable: false},
+			{Name: "description", DataType: "text", IsNullable: true},
+			{Name: "nullable", DataType: "varchar", IsNullable: false},
+			{Name: "changeable", DataType: "json", IsNullable: true},
+			{Name: "uniqueness", DataType: "varchar", IsNullable: false, IsUnique: false},
+			{Name: "identity", DataType: "varchar", IsNullable: false, IsIdentity: false},
+		},
+		Relationships: []objects.TablesRelationship{
+			{
+				ConstraintName:    "constraint1",
+				SourceSchema:      "public",
+				SourceTableName:   "table1",
+				SourceColumnName:  "id",
+				TargetTableSchema: "public",
+				TargetTableName:   "table2",
+				TargetColumnName:  "id",
+			},
+			{
+				ConstraintName:    "constraint2",
+				SourceSchema:      "public",
+				SourceTableName:   "table1",
+				SourceColumnName:  "name",
+				TargetTableSchema: "public",
+				TargetTableName:   "table2",
+				TargetColumnName:  "name",
+			},
+		},
+	}
+
+	diffResult := tables.CompareItem(source, target)
+	assert.True(t, diffResult.IsConflict)
+	assert.Equal(t, "table1", diffResult.SourceResource.Name)
+	assert.Equal(t, "table1_updated", diffResult.TargetResource.Name)
+	assert.Equal(t, []objects.UpdateColumnType{objects.UpdateColumnNullable}, diffResult.DiffItems.ChangeColumnItems[0].UpdateItems)
+	assert.Equal(t, []objects.UpdateColumnType{objects.UpdateColumnNullable}, diffResult.DiffItems.ChangeColumnItems[1].UpdateItems)
+}
